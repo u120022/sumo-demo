a641649ef0362b81da81223ab2993b09acbf008d
u120022 <u120022@st.pu-toyama.ac.jp>
2024-03-26 14:47:41 +0900

    support parallel dijkstra

--- macrosim/graph/src/main.rs
+++ macrosim/graph/src/main.rs
@@ -1,3 +1,5 @@
+const THREAD_COUNT: usize = 12;
+
 #[tokio::main]
 async fn main() {
     #[rustfmt::skip]
@@ -18,24 +20,13 @@ async fn main() {
         .await
         .unwrap();
 
-    let mut tree = rstar::RTree::new();
-
-    for node in &nodes {
-        tree.insert(rstar::primitives::GeomWithData::new(
-            [node.1, node.2],
-            node.0 as usize,
-        ));
-    }
-
-    println!("[tree stats] node: {}", tree.size());
-
     let mut graph = petgraph::Graph::<(f64, f64), f64, petgraph::Undirected>::new_undirected();
 
-    for node in &nodes {
+    for node in nodes {
         graph.add_node((node.1, node.2));
     }
 
-    for edge in &edges {
+    for edge in edges {
         let n1 = petgraph::graph::NodeIndex::new(edge.1 as usize - 1);
         let n2 = petgraph::graph::NodeIndex::new(edge.2 as usize - 1);
         let w = edge.3 / edge.4.unwrap_or(15.0).clamp(2.0, 15.0);
@@ -48,16 +39,29 @@ async fn main() {
         graph.edge_count()
     );
 
+    // maximum size graph only
+    let candidates = petgraph::algo::kosaraju_scc(&graph)
+        .into_iter()
+        .max_by_key(|nodes| nodes.len())
+        .unwrap();
+
+    let mut tree = rstar::RTree::new();
+    for candidate in candidates {
+        let node = graph.node_weight(candidate).unwrap();
+        tree.insert(rstar::primitives::GeomWithData::new(
+            [node.0, node.1],
+            candidate,
+        ));
+    }
+
     #[rustfmt::skip]
     let pairs: Vec<(i32, f64, f64, f64, f64)> = sqlx::query_as("SELECT id, ST_X(ST_StartPoint(geom)), ST_Y(ST_StartPoint(geom)), ST_X(ST_EndPoint(geom)), ST_Y(ST_EndPoint(geom)) FROM pair")
         .fetch_all(&pool)
         .await
         .unwrap();
 
-    println!("[pairs stats] pairs: {}", pairs.len());
-
     let mut plans = vec![];
-    for pair in &pairs {
+    for pair in pairs {
         let n1 = tree.nearest_neighbor(&[pair.1, pair.2]).unwrap().data;
         let n2 = tree.nearest_neighbor(&[pair.3, pair.4]).unwrap().data;
         plans.push((n1, n2));
@@ -65,22 +69,54 @@ async fn main() {
 
     println!("[plans stats] plans: {}", plans.len());
 
-    let indicator = indicatif::ProgressBar::new(plans.len() as u64);
+    let indicator = std::sync::Arc::new(indicatif::ProgressBar::new(plans.len() as u64));
+    let graph = std::sync::Arc::new(graph);
+    let split_len = plans.len().div_ceil(THREAD_COUNT);
+    let mut threads = vec![];
 
-    let mut paths = vec![];
-    for plan in &plans {
-        let n1 = petgraph::graph::NodeIndex::new(plan.0);
-        let n2 = petgraph::graph::NodeIndex::new(plan.1);
-        let path = petgraph::algo::dijkstra(&graph, n1, Some(n2), |e| *e.weight());
-
-        let path = path
-            .keys()
-            .map(|node| node.index() as i32)
+    for thread in 0..THREAD_COUNT {
+        let indicator = indicator.clone();
+        let graph = graph.clone();
+        let plans = plans
+            .iter()
+            .skip(split_len * thread)
+            .take(split_len)
+            .cloned()
             .collect::<Vec<_>>();
 
-        paths.push(path);
+        let thread = std::thread::spawn(move || {
+            let mut paths = vec![];
 
-        indicator.inc(1);
+            for plan in plans {
+                let path = petgraph::algo::astar(
+                    graph.as_ref(),
+                    plan.0,
+                    |n| n == plan.1,
+                    |e| *e.weight(),
+                    |_| 0.0,
+                );
+
+                if let Some((_, path)) = path {
+                    let path = path
+                        .into_iter()
+                        .map(|n| n.index() as i32)
+                        .collect::<Vec<_>>();
+
+                    paths.push(path);
+                }
+
+                indicator.inc(1);
+            }
+
+            paths
+        });
+        threads.push(thread);
+    }
+
+    let mut paths = vec![];
+    for thread in threads {
+        let mut split_paths = thread.join().unwrap();
+        paths.append(&mut split_paths);
     }
 
     indicator.finish();
